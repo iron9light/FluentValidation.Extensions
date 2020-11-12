ed2cbcd62275947e7d3203e9ca228a8b7abab916
iron9light <iron9light@gmail.com>
2020-11-12 21:10:05 +0800

    Support .NET 5.0

--- src/IL.FluentValidation.Extensions.Options.Tests/MyOptionsValidator.cs
+++ src/IL.FluentValidation.Extensions.Options.Tests/MyOptionsValidator.cs
@@ -2,9 +2,7 @@ using FluentValidation;
 
 namespace IL.FluentValidation.Extensions.Options.Tests
 {
-#pragma warning disable CA1710 // Identifiers should have correct suffix
     public class MyOptionsValidator
-#pragma warning restore CA1710 // Identifiers should have correct suffix
         : AbstractValidator<MyOptions>
     {
         public MyOptionsValidator()
